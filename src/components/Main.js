cffd2b0cd21ec97222decc334d837a02290364c1
shang-shang95 <qqxn7ra9k@gmail.com>
2023-09-17 23:01:31 +0900

    Update opacity of dragged task

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,8 +6,52 @@ import Card from "./Card";
 const Main = () => {
   const [data, setData] = useState(dummyData);
 
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+
+    if (source === null || destination === null) return;
+
+    // 別のカラムにタスクが移動した時
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
+      const destinationColIndex = data.findIndex(
+        (e) => e.id === destination.droppableId
+      );
+
+      const sourceCol = data[sourceColIndex];
+      const destinationCol = data[destinationColIndex];
+
+      const sourceTask = [...sourceCol.tasks];
+      const destinationTask = [...destinationCol.tasks];
+
+      const [removed] = sourceTask.splice(source.index, 1);
+
+      destinationTask.splice(destination.index, 0, removed);
+
+      data[sourceColIndex].tasks = sourceTask;
+      data[destinationColIndex].tasks = destinationTask;
+
+      setData(data);
+    } else {
+      // 同じカラムにタスクが移動した時
+      const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
+      const sourceCol = data[sourceColIndex];
+
+      const sourceTask = [...sourceCol.tasks];
+
+      // タスクを削除
+      const [removed] = sourceTask.splice(source.index, 1);
+      // タスクを追加
+      sourceTask.splice(destination.index, 0, removed);
+
+      data[sourceColIndex].tasks = sourceTask;
+
+      setData(data);
+    }
+  };
+
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={onDragEnd}>
       <div className="trello">
         {data.map((section) => (
           <Droppable key={section.id} droppableId={section.id}>
@@ -32,7 +76,7 @@ const Main = () => {
                           {...provided.dragHandleProps}
                           style={{
                             ...provided.draggableProps.style,
-                            opacity: snapshot.isDragging ? "0.3" : "1.0",
+                            opacity: snapshot.isDragging ? "0.4" : "1.0",
                           }}
                         >
                           <Card>{task.title}</Card>
